782fbaabe7eb4c2d4e4327bca55dae1b65a2a2c5
eric-muhire <77967086+eric-muhire@users.noreply.github.com>
2021-11-28 14:45:46 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "m_eric_instance" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
-  default     = "m_eric_instance
+  default     = "m_eric_instance"
 }
